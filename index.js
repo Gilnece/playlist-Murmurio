32ac30dd020b5685e59c34e87794c8ab41af4ed7
Gilnece Menezes <gilnece15@gmail.com>
2023-05-13 09:34:59 -0300

    Estou vivo

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const connetToDb = require("./database/db");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
 connetToDb();
 
-app.get("/hello", (req, res) =>{
-    res.send("Hello World!")
+app.get("/hello", (req, res) => {
+  res.render("index");
 });
 
-
-app.listen(port, () => 
-  console.log('Servidor rodando em http://localhost:${port}')
+app.listen(port, () =>
+  console.log("Servidor rodando em http://localhost:${port}")
 );
